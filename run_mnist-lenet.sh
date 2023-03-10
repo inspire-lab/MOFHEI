da2d1a4563d6a299bf465ed3bf6914a62f777c04
mhrgroup <info@mhrgroup.org>
2023-03-10 15:11:58 +0000

    updated run configs

--- run_mnist-lenet.sh
+++ run_mnist-lenet.sh
@@ -1,13 +1,13 @@
-python3 main_mlsurgery.py 2 0 -MA False -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.50
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.55
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.60
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.65
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.70
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.75
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.80
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.85
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.90
-python3 main_mlsurgery.py 2 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.95
+python3 main_mlsurgery.py 2 0 -MA False -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.50 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.55 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.60 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.65 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.70 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.75 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.80 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.85 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.90 -GP 1
+python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.95 -GP 1
 
 git fetch
 git pull
--- run_x-ray-modified-lenet.sh
+++ run_x-ray-modified-lenet.sh
@@ -1,13 +1,13 @@
-python3 main_mlsurgery.py 4 0 -MA False -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.50
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.55
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.60
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.65
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.70
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.75
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.80
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.85
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.90
-python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.95
+python3 main_mlsurgery.py 4 0 -MA False -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.50 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.55 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.60 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.65 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.70 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.75 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.80 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.85 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.90 -GP 3
+python3 main_mlsurgery.py 4 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 99 -OE 100 -TS 0.95 -GP 3
 
 git fetch
 git pull
