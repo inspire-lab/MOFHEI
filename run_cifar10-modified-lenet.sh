d63e87e8a7d8a5efa6ca41c19a85370891c6705d
Parsa Ghazvinian <parsa.ghz10@gmail.com>
2024-08-13 01:06:01 -0400

    Update run_cifar10-modified-lenet.sh

--- run_cifar10-modified-lenet.sh
+++ run_cifar10-modified-lenet.sh
@@ -1,13 +1,13 @@
-python3 main_mlsurgery.py 3 0 -MA False -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.50
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.55
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.60
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.65
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.70
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.75
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.80
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.85
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.90
-python3 main_mlsurgery.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.95
+python3 main_mofhei.py 3 0 -MA False -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.50
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.55
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.60
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.65
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.70
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.75
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.80
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.85
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.90
+python3 main_mofhei.py 3 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 50 -OE 100 -TS 0.95
 
 git fetch
 git pull
