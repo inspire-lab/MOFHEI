40024b89c642c1147af081e199cfe724ddb5e548
Parsa Ghazvinian <parsa.ghz10@gmail.com>
2024-08-13 14:56:42 -0400

    Update run_mnist-hepex-ae1.sh

--- run_mnist-hepex-ae1.sh
+++ run_mnist-hepex-ae1.sh
@@ -1,13 +1,13 @@
-python3 main_mlsurgery.py 5 1 -MA False -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.50
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.55
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.60
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.65
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.70
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.75
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.80
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.85
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.90
-python3 main_mlsurgery.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.95
+python3 main_mofhei.py 5 1 -MA False -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.50
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.55
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.60
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.65
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.70
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.75
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.80
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.85
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.90
+python3 main_mofhei.py 5 1 -MA True  -AD 0 -EO 20 -ET 50 -EF 50 -PE 10 -OE 5 -TS 0.95
 
 git fetch
 git pull
