666e09ade25f5150e801fa391b8aae343b16d917
Parsa Ghazvinian <parsa.ghz10@gmail.com>
2024-08-13 01:11:48 -0400

    Update run_mnist-lenet.sh

--- run_mnist-lenet.sh
+++ run_mnist-lenet.sh
@@ -1,13 +1,13 @@
-python3 main_mlsurgery.py 2 0 -MA False -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.50 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.55 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.60 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.65 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.70 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.75 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.80 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.85 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.90 -GP 1
-python3 main_mlsurgery.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.95 -GP 1
+python3 main_mofhei.py 2 0 -MA False -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.50 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.55 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.60 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.65 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.70 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.75 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.80 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.85 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.90 -GP 1
+python3 main_mofhei.py 2 0 -MA True  -AD 2 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.95 -GP 1
 
 git fetch
 git pull
