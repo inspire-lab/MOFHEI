59aa0d31e33e4ea438eb63d9041715f50c74f5d2
Parsa Ghazvinian <parsa.ghz10@gmail.com>
2024-08-13 01:12:38 -0400

    Update run_electrical-stability-fcnet.sh

--- run_electrical-stability-fcnet.sh
+++ run_electrical-stability-fcnet.sh
@@ -1,13 +1,13 @@
-python3 main_mlsurgery.py 1 0 -MA False -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.50
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.55
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.60
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.65
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.70
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.75
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.80
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.85
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.90
-python3 main_mlsurgery.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.95
+python3 main_mofhei.py 1 0 -MA False -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.50
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.55
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.60
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.65
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.70
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.75
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.80
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.85
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.90
+python3 main_mofhei.py 1 0 -MA True  -AD 0 -EO 100 -ET 100 -EF 100 -PE 10 -OE 100 -TS 0.95
 
 git fetch
 git pull
